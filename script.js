befd3c1e7df9251ca2585d59ec0df5106a4ca28b
Akshar-u-2077 <aksharu73@gmail.com>
2026-03-26 23:50:42 +0530

    Learning to add DOM and dynamic posts into the websites @ 11:50 pm

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function updateClock(){
 }
 updateClock();
 setInterval(updateClock,1000);
+
+
+let posts = [
+    {
+        title : '>_idk what i did',
+        date :'Feb 27,2026. 1:10 AM',
+        content : '>_yeh, idk why this shit came into my mind..'
+    },
+    {
+        title : '>_ post 2',
+        date :'Month DD,YYYY. HH:MM am/pm',
+        content : '>_ paragraph yet to be added'
+    }
+]
+let container = document.getElementById("container");
+for(let i=0;i<posts.length;i++)
+{
+    let article = document.createElement("article");
+    let title = document.createElement("h2");
+    title.textContent=posts[i].title;
+    article.appendChild(title);
+    let date = document.createElement("span");
+    date.textContent=posts[i].date;
+    article.appendChild(date);
+    let content = document.createElement("p");
+    content.textContent=posts[i].content;
+    article.appendChild(content);
+    container.appendChild(article);
+}
